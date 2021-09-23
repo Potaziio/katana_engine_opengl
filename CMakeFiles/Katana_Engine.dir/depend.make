01abe71abeea574605b375c463e5f63797281d84
Potaziio <estebitan836@gmail.com>
2021-09-23 19:27:04 -0400

    Removed more unnecessary files

--- CMakeFiles/Katana_Engine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Katana_Engine.
-# This may be replaced when dependencies are built.
--- CMakeFiles/imgui.dir/include/imgui/imgui_sdl.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/imgui.dir/include/imgui/imgui_sdl.cpp.o: \
- /home/potaziio-i3/Desktop/Coding/cpp-coding/opengl-sdl2-projects/first-test/include/imgui/imgui_sdl.cpp \
- /usr/include/stdc-predef.h
